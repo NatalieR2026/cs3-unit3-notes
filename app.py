c37348d521086db990be2c71b02fcadfd00de4a0
NatalieR2026 <natalie.radin2026@gbwl.org>
2026-02-09 17:41:46 +0000

    2/9

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Function that returns content
+# using the app.route decorator to map the URL
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# TO RUN YOUR APP enter "flask run" into the TERMINAL
+# (if you closed your terminal, open it again with CTRL + `)
+# TO STOP click CTRL + C in the TERMINAL
